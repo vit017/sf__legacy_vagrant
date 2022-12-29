b962bd9e41477328f8f2180b88c358d9984befff
vit017 <kaluzhskikh.v@gmail.com>
2022-12-29 21:14:29 +0530

    vagrant box remove

--- install_postgres.sh
+++ install_postgres.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install postgresql-8.4
+sudo apt-get install curl ca-certificates gnupg -y
+sudo curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install postgresql-8.4 -y
